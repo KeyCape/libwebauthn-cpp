244988b23fef6cb9372f7b4ed222a1fb789b5d40
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:43 +0100

    Implement json deserializer

--- src/AuthenticatorAttestationResponse.cpp
+++ src/AuthenticatorAttestationResponse.cpp
@@ -7,9 +7,25 @@ AuthenticatorAttestationResponse::AuthenticatorAttestationResponse(
     : attestationObject{attObj}, AuthenticatorResponse{
                                      std::move(clientDataJSON)} {}
 
-std::shared_ptr<AuthenticatorAttestationResponse>
-AuthenticatorAttestationResponse::fromJson(const std::string &json) {
-    return std::make_shared<AuthenticatorAttestationResponse>();
+void AuthenticatorAttestationResponse::fromJson(
+    const std::shared_ptr<Json::Value> json) {
+  if (json->isNull()) {
+    throw std::invalid_argument{"Empty json"};
+  }
+  if (json->isMember("response")) {
+    if (!(*json)["response"].isMember("attestationObject")) {
+      throw std::invalid_argument{"Missing key attestationObject"};
+    }
+  } else {
+    throw std::invalid_argument{"Missing key: response"};
+  }
+  std::string tmp = (*json)["response"]["attestationObject"].asString();
+
+  this->attestationObject.reserve(tmp.size());
+  std::transform(tmp.begin(), tmp.end(), this->attestationObject.begin(),
+                 [](const auto &t) { return t; });
+
+  AuthenticatorResponse::fromJson(json);
 }
 
 AuthenticatorAttestationResponse::~AuthenticatorAttestationResponse() {}
