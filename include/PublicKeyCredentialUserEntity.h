8168ecc5db61005bec9c95acf733c80f2e78a841
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:42 +0100

    Create PublicKeyCredentialUserEntity class

---
+++ include/PublicKeyCredentialUserEntity.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "PublicKeyCredentialEntity.h"
+#include <string>
+
+/// @brief https://w3c.github.io/webauthn/#dictionary-user-credential-params
+class PublicKeyCredentialUserEntity : public PublicKeyCredentialEntity {
+protected:
+  std::string displayName;
+  std::string id;
+
+public:
+  PublicKeyCredentialUserEntity() = delete;
+  PublicKeyCredentialUserEntity(std::string &&name, std::string &&displayName,
+                                std::string &&id);
+  virtual std::unique_ptr<Json::Value> getJson() override;
+  ~PublicKeyCredentialUserEntity();
+};
