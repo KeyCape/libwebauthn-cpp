32a7beb395817a656cc47942c3775ea5ec1aeb11
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:43 +0100

    Add deserialization

--- include/PublicKeyCredential.h
+++ include/PublicKeyCredential.h
@@ -1,21 +1,29 @@
 #pragma once
-#include "AuthenticatorResponse.h"
+#include "AuthenticatorAttestationResponse.h"
+#include "IJsonDeserialize.h"
 #include <memory>
 #include <string>
 #include <vector>
 #include <cstdint>
+#include <jsoncpp/json/value.h>
+#include <jsoncpp/json/reader.h>
 
-class PublicKeyCredential {
+class PublicKeyCredential : IJsonDeserialize<PublicKeyCredential> {
 protected:
   std::string id;
   std::string type;
   std::vector<std::uint8_t> rawId;
-  std::shared_ptr<AuthenticatorResponse> response;
+  std::shared_ptr<AuthenticatorAttestationResponse> response;
 
 public:
-  PublicKeyCredential() = delete;
+  PublicKeyCredential();
   PublicKeyCredential(std::string &&id, std::string &&type,
                       std::vector<std::uint8_t> &&rawId,
-                      std::shared_ptr<AuthenticatorResponse> response);
+                      std::shared_ptr<AuthenticatorAttestationResponse> response);
+  virtual std::shared_ptr<PublicKeyCredential> fromJson(const std::string &json) override;
+  const std::string& getId();
+  const std::string& getType();
+  const std::vector<std::uint8_t>& getRawId();
+  const std::shared_ptr<AuthenticatorAttestationResponse> getResponse();
   ~PublicKeyCredential();
 };
--- src/PublicKeyCredential.cpp
+++ src/PublicKeyCredential.cpp
@@ -1,8 +1,42 @@
 #include "PublicKeyCredential.h"
 
+PublicKeyCredential::PublicKeyCredential()
+    : id{""}, type{""}, rawId{0}, response{nullptr} {}
+
 PublicKeyCredential::PublicKeyCredential(
     std::string &&id, std::string &&type, std::vector<std::uint8_t> &&rawId,
-    std::shared_ptr<AuthenticatorResponse> response)
+    std::shared_ptr<AuthenticatorAttestationResponse> response)
     : id{id}, type{type}, rawId{rawId}, response{response} {}
+
+std::shared_ptr<PublicKeyCredential> PublicKeyCredential::fromJson(const std::string &json) {
+  JSONCPP_STRING err;
+  Json::Value root;
+  Json::CharReaderBuilder builder;
+  const std::unique_ptr<Json::CharReader> reader{builder.newCharReader()};
+
+  if (!reader->parse(json.c_str(), json.c_str() + json.length(), &root, &err)) {
+    throw std::runtime_error{err};
+  }
+
+  std::string rawIdTemp = root["id"].asString();
+  std::vector<std::uint8_t> rawId(rawIdTemp.length());
+  std::transform(rawIdTemp.begin(), rawIdTemp.end(), rawId.begin(),
+                 [](unsigned char c) { return c; });
+
+  std::shared_ptr<AuthenticatorAttestationResponse> aatr;
+
+  return std::make_shared<PublicKeyCredential>(root["id"].asString(), root["type"].asString(),
+                                 std::move(rawId), aatr);
+}
+
+const std::string &PublicKeyCredential::getId() { return this->id; }
+const std::string &PublicKeyCredential::getType() { return this->type; }
+const std::vector<std::uint8_t> &PublicKeyCredential::getRawId() {
+  return this->rawId;
+}
+const std::shared_ptr<AuthenticatorAttestationResponse>
+PublicKeyCredential::getResponse() {
+  return this->response;
+}
 
 PublicKeyCredential::~PublicKeyCredential() {}
