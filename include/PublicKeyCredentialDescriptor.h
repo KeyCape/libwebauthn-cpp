dcf0f2f8245a91837eb287557491d8129d596ba5
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:44 +0100

    Inherit from IJsonSerialize

--- include/PublicKeyCredentialDescriptor.h
+++ include/PublicKeyCredentialDescriptor.h
@@ -1,17 +1,20 @@
 #pragma once
+#include "IJsonSerialize.h"
 #include <string>
 
 /**
  * @brief https://w3c.github.io/webauthn/#dictionary-credential-descriptor
- * 
+ *
  * This Implementation does not use the transports attribute.
  */
-class PublicKeyCredentialDescriptor {
+class PublicKeyCredentialDescriptor : public IJsonSerialize {
 private:
-std::string type;
-std::string id;
+  std::string type;
+  std::string id;
+
 public:
   PublicKeyCredentialDescriptor() = delete;
   PublicKeyCredentialDescriptor(std::string &&type, std::string &&id);
+  virtual std::unique_ptr<Json::Value> getJson() override;
   ~PublicKeyCredentialDescriptor();
 };
--- src/PublicKeyCredentialDescriptor.cpp
+++ src/PublicKeyCredentialDescriptor.cpp
@@ -4,4 +4,11 @@ PublicKeyCredentialDescriptor::PublicKeyCredentialDescriptor(std::string &&type,
                                                              std::string &&id)
     : type{type}, id{id} {}
 
+std::unique_ptr<Json::Value> PublicKeyCredentialDescriptor::getJson() {
+  auto val = std::make_unique<Json::Value>();
+  (*val)["id"] = this->id;
+  (*val)["type"] = this->type;
+  return val;
+}
+
 PublicKeyCredentialDescriptor::~PublicKeyCredentialDescriptor() {}
