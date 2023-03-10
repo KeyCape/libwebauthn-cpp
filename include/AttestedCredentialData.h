692a95babba49418bb89bcc3c371154a7aa5c9bf
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:44 +0100

    Add implementation for EC extraction

--- include/AttestedCredentialData.h
+++ include/AttestedCredentialData.h
@@ -1,10 +1,11 @@
 #pragma once
-#include <string>
-#include <vector>
-#include <memory>
+#include "PublicKeyCredentialParameters.h"
+#include <cbor.h>
 #include <cstring>
 #include <glog/logging.h>
-#include <cbor.h>
+#include <memory>
+#include <string>
+#include <vector>
 
 /**
  * @brief Attested credential data is a variable-length byte array added to the
@@ -13,14 +14,74 @@
  * §6.5.2
  *
  */
+struct PublicKey;
 class AttestedCredentialData {
 private:
-std::shared_ptr<std::string> aaguid;
-uint16_t credentialIdLength;
-std::shared_ptr<std::string> credentialId;
+  std::shared_ptr<std::string> aaguid;
+  uint16_t credentialIdLength;
+  std::shared_ptr<std::string> credentialId;
+  std::shared_ptr<PublicKey>
+      pkey; // The authenticators public key, which is used to sign
 
-void extractCredentialPublicKey(const std::vector<unsigned char> &attCredData);
+  void
+  extractCredentialPublicKey(const std::vector<unsigned char> &attCredData);
+  void jumpToMapLabel(int &&label, CborValue *it);
+  void storePublicKeyEC2(CborValue &map);
+
 public:
   AttestedCredentialData(const std::vector<unsigned char> &attCredData);
   ~AttestedCredentialData();
+};
+
+/**
+ * @brief See: https://www.iana.org/assignments/cose/cose.xhtml
+ *
+ * Section COSE Key Types
+ *
+ */
+enum COSEKeyType {
+  RESERVED = 0,  // This value is reserved
+  OKP = 1,       // Octet Key Pair
+  EC2 = 2,       // Elliptic Curve Keys w/ x- and y-coordinate pair
+  RSA = 3,       // RSA Key
+  SYMMETRIC = 4, // Symmetric Keys
+  HSS_LMS = 5,   // Public key for HSS/LMS hash-based digital signature
+  WALNUT_DSA = 6 // WalnutDSA public key
+};
+
+/**
+ * @brief Public key base class. This struct is not meant to be used standalone,
+ * but to be derived from. See PublicKeyEC2.
+ *
+ */
+struct PublicKey {
+  COSEKeyType kty;             // Identification of the key type
+  COSEAlgorithmIdentifier alg; // Key usage restriction to this algorithm
+};
+
+/**
+ * @brief Public key type for elliptic curves
+ * Notice, that the attribute d(private key) is missing. The attribute d is not
+ * used on the server side.
+ *
+ */
+struct PublicKeyEC2 : public PublicKey {
+  PublicKeyEC2()  {
+    this->kty = COSEKeyType::EC2;
+  }
+  int crv; // EC identifier -- Taken from the "COSE Elliptic Curves" registry
+  std::vector<char> x; // x-coordinate
+  std::vector<char> y; // y-coordinate
+};
+
+/**
+ * @brief Public key type for RSA
+ *
+ * Notice, that not all available attributes from [RFC9053] are implemented,
+ * because the are not used here.
+ *
+ */
+struct PublicKeyRSA : public PublicKey {
+  std::vector<char> n; // The RSA modulus n
+  std::vector<char> e; // The RSA public exponent e
 };
--- src/AttestedCredentialData.cpp
+++ src/AttestedCredentialData.cpp
@@ -80,80 +80,265 @@ void AttestedCredentialData::extractCredentialPublicKey(
         "Couldn't parse the attestationObject with cbor. CborError: "}
                                     .append(cbor_error_string(err))};
   }
-  size_t len = 0;
-  cbor_value_get_map_length(&map, &len);
-  LOG(INFO) << "Cbor map len: " << len;
+  // Check if the cbor is of type map
   LOG(INFO) << "Check if the attestationObject is a map";
   if (!cbor_value_is_map(&map)) {
     throw std::invalid_argument{
         "The attestationObject has to be a cbor map, but is from type: " +
         cbor_value_get_type(&map)};
   }
-  LOG(INFO) << "Is map array?: " << cbor_value_is_array(&map);
 
+  // Get the length of the map
+  size_t len = 0;
+  cbor_value_get_map_length(&map, &len);
+  LOG(INFO) << "Cbor map len: " << len;
+
+  // Temporary vars
   CborValue cval;
   size_t clen;
 
+  // Use an iterator to move between the fields of the map.
+  // Unfortunately we can't use the key finder function to extract the fields,
+  // because the COSE_Key format is used
   err = cbor_value_enter_container(&map, &cval);
   if (err != CborNoError) {
     throw std::invalid_argument{std::string{
         "Error occured during the enter container proccess. CborError: "}
                                     .append(cbor_error_string(err))};
   }
-  LOG(INFO) << "It type: " << cbor_value_get_type(&cval);
-  int res = 0;
-  cbor_value_get_int(&cval, &res);
-  LOG(INFO) << "Value: " << res;
 
-  err = cbor_value_advance(&cval);
-  if (err != CborNoError) {
+  // 1. Iterate through the cbor map until the kty(COSEKeyType) is found.
+  LOG(INFO) << "Extracting the key type";
+  this->jumpToMapLabel(1, &cval);
+  if (cbor_value_at_end(&cval)) {
+    throw std::invalid_argument{"Missing the key type.(COSE_KEY)"};
+  }
+
+  int keyType = 0;
+  switch (cbor_value_get_type(&cval)) {
+  case CborIntegerType:
+    if ((err = cbor_value_get_int(&cval, &keyType)) != CborNoError) {
+      LOG(ERROR) << "An unexpected error occured while getting the key type. "
+                    "CborError: "
+                 << cbor_error_string(err);
+      std::runtime_error{
+          "An unexpected error occured while getting the key type"};
+    }
+    break;
+  case CborTextStringType:
+    LOG(WARNING)
+        << "The key type value has to int. STRINGS ARE NOT IMPLEMENTED YET!";
     throw std::invalid_argument{
-        std::string{"Error occured during the advance it proccess. CborError: "}
-            .append(cbor_error_string(err))};
+        "The key type value has to int. STRINGS ARE NOT IMPLEMENTED YET!"};
+  default:
+    LOG(WARNING) << "Invalid key type value type";
+    throw std::invalid_argument{"Invalid key type type"};
   }
 
-  LOG(INFO) << "It type: " << cbor_value_get_type(&cval);
-  LOG(INFO) << "Is tag: " << cbor_value_is_tag(&cval);
-  cbor_value_get_int(&cval, &res);
-  LOG(INFO) << "Value: " << res;
-
-  err = cbor_value_advance(&cval);
-  if (err != CborNoError) {
-    throw std::invalid_argument{
-        std::string{"Error occured during the advance it proccess. CborError: "}
-            .append(cbor_error_string(err))};
-  }
-
-  LOG(INFO) << "It type: " << cbor_value_get_type(&cval);
-  LOG(INFO) << "Is tag: " << cbor_value_is_tag(&cval);
-  cbor_value_get_int(&cval, &res);
-  LOG(INFO) << "Value: " << res;
-
-  err = cbor_value_advance(&cval);
-  if (err != CborNoError) {
-    throw std::invalid_argument{
-        std::string{"Error occured during the advance it proccess. CborError: "}
-            .append(cbor_error_string(err))};
-  }
-
-  LOG(INFO) << "It type: " << cbor_value_get_type(&cval);
-  LOG(INFO) << "Is tag: " << cbor_value_is_tag(&cval);
-  cbor_value_get_int(&cval, &res);
-  LOG(INFO) << "Value: " << res;
-
-  LOG(INFO) << "Search after the key type";
-  // Field 1
-  err = cbor_value_map_find_value(&map, "1", &cval);
-  if (err != CborNoError) {
-    throw std::invalid_argument{std::string{
-        "Error occured during the find proccess of the 1 field. CborError: "}
-                                    .append(cbor_error_string(err))};
-  }
-  LOG(INFO) << "Check if the field 1 exists";
-  // Check if the field fmt is missing
-  if (cbor_value_get_type(&cval) == CborInvalidType) {
-    throw std::invalid_argument{"attestationObject is missing the field 1"};
+  // Checking if the COSEKeyType is valid
+  switch (keyType) {
+  case COSEKeyType::EC2:
+    LOG(INFO) << "The COSEKeyType is EC2";
+    this->storePublicKeyEC2(map);
+    break;
+  case COSEKeyType::RSA:
+    LOG(INFO) << "The COSEKeyType is RSA";
+    break;
+  default:
+    LOG(ERROR) << "The COSEKeyType is not supported. COSEKeyType: " << keyType;
+    throw std::invalid_argument{"The COSEKeyType is not supported"};
   }
 }
 
+/**
+ * @brief Jumps to a field with the specified label
+ *
+ * @param label Label as int
+ * @param it Iterator has to be a iterator pointing to the first label
+ */
+void AttestedCredentialData::jumpToMapLabel(int &&label, CborValue *it) {
+  CborError err;
+  for (unsigned int idx = 0; !cbor_value_at_end(it); ++idx) {
+    // If the index points to the label
+    if (idx % 2 == 0) {
+      // The label can either be an int or string
+      int tmpLabel = 0;
+      switch (cbor_value_get_type(it)) {
+      case CborType::CborIntegerType:
+        if ((err = cbor_value_get_int(it, &tmpLabel)) != CborNoError) {
+          throw std::invalid_argument{std::string{
+              "An error occured while reading the label from the COSE_KEY map"}
+                                          .append(cbor_error_string(err))};
+        }
+        // Checks if the label is found
+        if (tmpLabel == label) {
+          // Move to the value
+          if ((err = cbor_value_advance(it)) != CborNoError) {
+            LOG(ERROR) << "An unexpected error occured while iterating over "
+                          "the cbor map. CborError: "
+                       << cbor_error_string(err);
+            throw std::runtime_error{"An unexpected error occured."};
+          }
+          return;
+        }
+        break;
+      case CborType::CborTextStringType:
+        // TODO
+        throw std::invalid_argument{
+            "The label type has to be int and not a text string. THIS IS NOT "
+            "IMPLEMENTED YET!"};
+      default:
+        throw std::invalid_argument{
+            "The cbor format is not compliant. Wrong label type."};
+      }
+      if ((err = cbor_value_advance(it)) != CborNoError) {
+        LOG(ERROR) << "An unexpected error occured while iterating over the "
+                      "cbor map. CborError: "
+                   << cbor_error_string(err);
+        throw std::runtime_error{"An unexpected error occured."};
+      }
+    }
+  }
+}
+
+void AttestedCredentialData::storePublicKeyEC2(CborValue &map) {
+  auto pkPtr = std::make_shared<PublicKeyEC2>();
+  CborValue it;
+  CborError err;
+  // If all 4 bits are set to true, then all parameters has been set. Else some
+  // are missing
+  std::bitset<4> check{0};
+
+  if ((err = cbor_value_enter_container(&map, &it)) != CborNoError) {
+    LOG(ERROR)
+        << "Error occured during the enter container proccess. CborError: "
+        << cbor_error_string(err);
+    throw std::invalid_argument{std::string{
+        "Error occured during the enter container proccess. CborError: "}
+                                    .append(cbor_error_string(err))};
+  }
+
+  // Iterate through all fields
+  for (unsigned int idx = 0; !cbor_value_at_end(&it); ++idx) {
+    switch (cbor_value_get_type(&it)) {
+    case CborIntegerType:
+      int tmpLabel = 0;
+      if ((err = cbor_value_get_int(&it, &tmpLabel)) != CborNoError) {
+        LOG(ERROR) << "An unexpected error occured during cbor_value_get_int() "
+                      "of the label. CborError: "
+                   << cbor_error_string(err);
+        throw std::runtime_error{"An unexpected error occured"};
+      }
+      if ((err = cbor_value_advance(&it)) != CborNoError) {
+        LOG(ERROR) << "An unexpected error occured during cbor_value_advance() "
+                      "CborError: "
+                   << cbor_error_string(err);
+        throw std::runtime_error{"An unexpected error occured"};
+      }
+      switch (tmpLabel) {
+      case 3: // Algorithm
+      {
+        LOG(INFO) << "Extract the algorithm";
+        check.set(0);
+        if (cbor_value_get_type(&it) != CborIntegerType) {
+          LOG(WARNING) << "The algorithm value has to be an int";
+          throw std::invalid_argument{
+              "The type of the field 3(alg) has to be an int"};
+        }
+        int alg = 0;
+        cbor_value_get_int(&it, &alg);
+        pkPtr->alg = static_cast<COSEAlgorithmIdentifier>(alg);
+        break;
+      }
+      case -1: // Curve
+        LOG(INFO) << "Extract the curve";
+        check.set(1);
+        if (cbor_value_get_type(&it) != CborIntegerType) {
+          LOG(WARNING) << "The curve value has to be an int";
+          throw std::invalid_argument{
+              "The type of the field -1(curve) has to be an int"};
+        }
+        cbor_value_get_int(&it, &pkPtr->crv);
+        break;
+      case -2: // X-coordinate
+      {
+        LOG(INFO) << "Extract the x-coordinate";
+        check.set(2);
+        size_t tmpLen = 0;
+        if (!cbor_value_is_byte_string(&it)) {
+          LOG(WARNING) << "The x-coordinate has to be a bytestring";
+          throw std::invalid_argument{
+              "The x-coordinate has to be a bytestring"};
+        }
+        if ((err = cbor_value_calculate_string_length(&it, &tmpLen)) !=
+            CborNoError) {
+          LOG(ERROR) << "An unexpected error occured while calculating the "
+                        "length of the x-coordinate. CborError: "
+                     << cbor_error_string(err);
+          throw std::runtime_error{
+              "An unexpected error occured while calculating the length of the "
+              "x-coordinate."};
+        }
+        DLOG(INFO) << "The x-coordinate has a length of: " << tmpLen;
+        pkPtr->x.resize(tmpLen);
+        if ((err = cbor_value_copy_byte_string(&it, (uint8_t *)pkPtr->x.data(),
+                                               &tmpLen, NULL)) != CborNoError) {
+          LOG(ERROR) << "An unexpected error occured while copying the "
+                        "x-coordinate into the vector. CborError: "
+                     << cbor_error_string(err);
+          throw std::runtime_error{"An unexpected error occured while copying "
+                                   "the x-coordinate into the vector"};
+        }
+        break;
+      }
+      case -3: // Y-coordinate
+      {
+        LOG(INFO) << "Extract the y-coordinate";
+        check.set(3);
+        size_t tmpLen = 0;
+        if (!cbor_value_is_byte_string(&it)) {
+          LOG(WARNING) << "The y-coordinate has to be a bytestring";
+          throw std::invalid_argument{
+              "The y-coordinate has to be a bytestring"};
+        }
+        if ((err = cbor_value_calculate_string_length(&it, &tmpLen)) !=
+            CborNoError) {
+          LOG(ERROR) << "An unexpected error occured while calculating the "
+                        "length of the y-coordinate. CborError: "
+                     << cbor_error_string(err);
+          throw std::runtime_error{
+              "An unexpected error occured while calculating the length of the "
+              "y-coordinate."};
+        }
+        DLOG(INFO) << "The y-coordinate has a length of: " << tmpLen;
+        pkPtr->y.resize(tmpLen);
+        if ((err = cbor_value_copy_byte_string(&it, (uint8_t *)pkPtr->y.data(),
+                                               &tmpLen, NULL)) != CborNoError) {
+          LOG(ERROR) << "An unexpected error occured while copying the "
+                        "y-coordinate into the vector. CborError: "
+                     << cbor_error_string(err);
+          throw std::runtime_error{"An unexpected error occured while copying "
+                                   "the y-coordinate into the vector"};
+        }
+        break;
+      }
+      default: // Ignoring the rest
+        break;
+      }
+      if ((err = cbor_value_advance(&it)) != CborNoError) {
+        LOG(ERROR) << "An unexpected error occured during cbor_value_advance() "
+                      "CborError: "
+                   << cbor_error_string(err);
+        throw std::runtime_error{"An unexpected error occured"};
+      }
+    }
+  }
+
+  // Check if all fields were found
+  if (!check.all()) {
+    LOG(WARNING) << "Not all fields for the specified cipher has been set";
+    throw std::invalid_argument{"Missing fields for the cipher"};
+  }
+  LOG(INFO) << "Cipher field status check passed";
+}
 AttestedCredentialData::~AttestedCredentialData() {}
