ef723550f1902c3e84124c1cb397c909e60d18ea
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:42 +0100

    Remove json layer

--- src/PublicKeyCredentialEntity.cpp
+++ src/PublicKeyCredentialEntity.cpp
@@ -1,14 +1,12 @@
 #include <PublicKeyCredentialEntity.h>
 #include <mutex>
 
-#define CLASS_NAME "PublicKeyCredentialEntity"
-
 PublicKeyCredentialEntity::PublicKeyCredentialEntity(std::string &&name)
     : name{name} {}
 
 std::unique_ptr<Json::Value> PublicKeyCredentialEntity::getJson() {
   auto json = std::make_unique<Json::Value>();
-  (*json)[CLASS_NAME]["name"] = this->name;
+  (*json)["name"] = this->name;
 
   return json;
 }
