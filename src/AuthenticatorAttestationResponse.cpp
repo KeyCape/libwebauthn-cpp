7ade1e4666fe8c146096a8d9bdfc1053864b923b
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:43 +0100

    Add deserialization

--- src/AuthenticatorAttestationResponse.cpp
+++ src/AuthenticatorAttestationResponse.cpp
@@ -1,8 +1,15 @@
 #include "AuthenticatorAttestationResponse.h"
 
+AuthenticatorAttestationResponse::AuthenticatorAttestationResponse() {}
+
 AuthenticatorAttestationResponse::AuthenticatorAttestationResponse(
     std::vector<uint8_t> &&attObj, std::vector<uint8_t> &&clientDataJSON)
     : attestationObject{attObj}, AuthenticatorResponse{
                                      std::move(clientDataJSON)} {}
+
+std::shared_ptr<AuthenticatorAttestationResponse>
+AuthenticatorAttestationResponse::fromJson(const std::string &json) {
+    return std::make_shared<AuthenticatorAttestationResponse>();
+}
 
 AuthenticatorAttestationResponse::~AuthenticatorAttestationResponse() {}
