172038f8d7fec42b39f9ba1e923c0039eab09df6
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:42 +0100

    Implement PublicKeyCredentialParameters class

--- include/PublicKeyCredentialParameters.h
+++ include/PublicKeyCredentialParameters.h
@@ -1,9 +1,34 @@
 #pragma once
+#include <string>
 
-class PublicKeyCredentialParameters
-{
+/**
+ * @brief https://w3c.github.io/webauthn/#typedefdef-cosealgorithmidentifier
+ *
+ * This enumeration is used to restrict the algorithm kobinations used by the
+ * client.
+ *
+ */
+enum COSEAlgorithmIdentifier {
+  ES256 = -7,
+  ES384 = -35,
+  ES512 = -36,
+  EDDSA = -8,
+  ED25519 = 6,
+  P256 = 1,
+  P384 = 2,
+  P521 = 3
+};
+
+enum PublicKeyCredentialType { public_key };
+
+class PublicKeyCredentialParameters {
 private:
+  std::string type;
+  COSEAlgorithmIdentifier alg;
+
 public:
-    PublicKeyCredentialParameters();
-    ~PublicKeyCredentialParameters();
+  PublicKeyCredentialParameters(
+      COSEAlgorithmIdentifier &&alg,
+      PublicKeyCredentialType &&type = PublicKeyCredentialType::public_key);
+  ~PublicKeyCredentialParameters();
 };
--- src/PublicKeyCredentialParameters.cpp
+++ src/PublicKeyCredentialParameters.cpp
@@ -1,11 +1,13 @@
 #include "PublicKeyCredentialParameters.h"
 
-PublicKeyCredentialParameters::PublicKeyCredentialParameters()
-{
-
+PublicKeyCredentialParameters::PublicKeyCredentialParameters(
+    COSEAlgorithmIdentifier &&alg, PublicKeyCredentialType &&type)
+    : alg{alg} {
+  switch (type) {
+  case public_key:
+    this->type = "public-key";
+    break;
+  }
 }
 
-PublicKeyCredentialParameters::~PublicKeyCredentialParameters()
-{
-
-}
+PublicKeyCredentialParameters::~PublicKeyCredentialParameters() {}
