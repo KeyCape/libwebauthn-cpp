390e22598f6ca72979c7e0f54aa88a7dbea0f6d6
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:42 +0100

    Add challenge signatures

--- include/Challenge.h
+++ include/Challenge.h
@@ -1,16 +1,18 @@
 #pragma once
+#include "IJsonSerialize.h"
 #include <bitset>
 #include <memory>
 #include <random>
 
 #define BYTE_LEN 16
 
-class Challenge {
+class Challenge : IJsonSerialize {
 private:
-  std::shared_ptr<std::vector<char>> challenge;
+  std::shared_ptr<std::vector<unsigned char>> challenge;
 
 public:
   Challenge();
-  std::shared_ptr<std::vector<char>> getChallenge();
+  std::shared_ptr<std::vector<unsigned char>> getChallenge();
+  virtual std::unique_ptr<Json::Value>getJson() override;
   ~Challenge();
 };
--- src/Challenge.cpp
+++ src/Challenge.cpp
@@ -1,17 +1,36 @@
 #include "Challenge.h"
 #include <algorithm>
+#include <jsoncpp/json/value.h>
+#include <drogon/utils/Utilities.h>
 #include <random>
 
 Challenge::Challenge() {
   std::random_device rd;
   std::independent_bits_engine<std::mt19937_64, 8, unsigned int> e1{rd()};
 
-  this->challenge = std::make_shared<std::vector<char>>(BYTE_LEN);
+  this->challenge = std::make_shared<std::vector<unsigned char>>(BYTE_LEN);
   std::generate(this->challenge->begin(), this->challenge->end(), e1);
 }
 
-std::shared_ptr<std::vector<char>> Challenge::getChallenge() {
+std::shared_ptr<std::vector<unsigned char>> Challenge::getChallenge() {
   return this->challenge;
+}
+std::unique_ptr<Json::Value> Challenge::getJson() {
+  auto ret = std::make_unique<Json::Value>(Json::objectValue);
+
+  // Convert the vector to a string
+  std::basic_string<unsigned char> str{this->challenge->begin(),
+                                       this->challenge->end()};
+
+  // Encode the challenge to base64
+
+  /* TODO
+   * Die Präprozessor DIrektive __cplusplus ist zu alt. Scheinbar existieren diverse Versionen.
+   * Um die Auflösung von boost zu verhindern muss der Compiler auf die neueste Version aktualisiert werden.
+  */
+  (*ret)["challenge"] = drogon::utils::base64Encode(str.c_str(), str.size());
+
+  return ret;
 }
 
 Challenge::~Challenge() {}
