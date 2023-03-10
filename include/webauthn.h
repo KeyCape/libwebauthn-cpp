4e35baba6413530d1697c7d165bd27543257c837
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:42 +0100

    Add relying party attributes

--- include/webauthn.h
+++ include/webauthn.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <string>
 
 class Webauthn {
+private:
+  std::string rp_name;
+  std::string rp_id;
+
 public:
   Webauthn();
+  /// @brief Create an instance of a relying party
+  /// @param name This is the name of the relying party
+  /// @param id This is the id of the relying party, which is transfered to the
+  /// WebAgent
+  Webauthn(std::string &&name, std::string &&id);
   ~Webauthn();
 };
