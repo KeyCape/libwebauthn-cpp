0fcdf09d7767e85547d6c490df4512f90c469253
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:42 +0100

    Add constructor

--- include/PublicKeyCredentialCreationOptions.h
+++ include/PublicKeyCredentialCreationOptions.h
@@ -1,8 +1,41 @@
 #pragma once
+#include "AuthenticatorSelectionCriteria.h"
+#include "Challenge.h"
+#include "PublicKeyCredentialDescriptor.h"
+#include "PublicKeyCredentialParameters.h"
+#include "PublicKeyCredentialRpEntity.h"
+#include "PublicKeyCredentialUserEntity.h"
+#include <forward_list>
 
+/**
+ * @brief https://w3c.github.io/webauthn/#dictionary-makecredentialoptions
+ *
+ * The extensions attribute unsued.
+ *
+ */
 class PublicKeyCredentialCreationOptions {
 private:
+  std::shared_ptr<PublicKeyCredentialRpEntity> rp;
+  std::shared_ptr<PublicKeyCredentialUserEntity> user;
+
+  std::shared_ptr<Challenge> challenge;
+  std::shared_ptr<std::forward_list<PublicKeyCredentialParameters>>
+      pubKeyCredParams;
+
+  unsigned long timeout;
+  std::shared_ptr<std::forward_list<PublicKeyCredentialDescriptor>>
+      excludeCredentials;
+  std::shared_ptr<AuthenticatorSelectionCriteria> authenticatorSelection;
+  std::string attestation = "none";
+  std::shared_ptr<std::forward_list<std::string>> attestationFormats;
+
 public:
-  PublicKeyCredentialCreationOptions();
+  PublicKeyCredentialCreationOptions() = delete;
+  PublicKeyCredentialCreationOptions(
+      std::shared_ptr<PublicKeyCredentialRpEntity> &rp,
+      std::shared_ptr<PublicKeyCredentialUserEntity> &user,
+      std::shared_ptr<Challenge> &challenge,
+      std::shared_ptr<std::forward_list<PublicKeyCredentialParameters>>
+          &pubKeyCredParams);
   ~PublicKeyCredentialCreationOptions();
 };
--- src/PublicKeyCredentialCreationOptions.cpp
+++ src/PublicKeyCredentialCreationOptions.cpp
@@ -1,5 +1,11 @@
 #include "PublicKeyCredentialCreationOptions.h"
 
-PublicKeyCredentialCreationOptions::PublicKeyCredentialCreationOptions() {}
+PublicKeyCredentialCreationOptions::PublicKeyCredentialCreationOptions(
+    std::shared_ptr<PublicKeyCredentialRpEntity> &rp,
+    std::shared_ptr<PublicKeyCredentialUserEntity> &user,
+    std::shared_ptr<Challenge> &challenge,
+    std::shared_ptr<std::forward_list<PublicKeyCredentialParameters>>
+        &pubKeyCredParams)
+    : rp{rp}, user{user}, challenge{challenge} {}
 
 PublicKeyCredentialCreationOptions::~PublicKeyCredentialCreationOptions() {}
