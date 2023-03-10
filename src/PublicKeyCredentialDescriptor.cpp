34354472f4074873ed9eea3a5f4b081e8287c7b8
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:45 +0100

    Encode id

--- src/PublicKeyCredentialDescriptor.cpp
+++ src/PublicKeyCredentialDescriptor.cpp
@@ -1,4 +1,5 @@
 #include "PublicKeyCredentialDescriptor.h"
+#include <drogon/utils/Utilities.h>
 
 PublicKeyCredentialDescriptor::PublicKeyCredentialDescriptor(std::string &&type,
                                                              std::string &&id)
@@ -6,7 +7,9 @@ PublicKeyCredentialDescriptor::PublicKeyCredentialDescriptor(std::string &&type,
 
 std::unique_ptr<Json::Value> PublicKeyCredentialDescriptor::getJson() {
   auto val = std::make_unique<Json::Value>();
-  (*val)["id"] = this->id;
+
+  (*val)["id"] = drogon::utils::base64Encode(
+      (const unsigned char *)this->id.c_str(), this->id.size());
   (*val)["type"] = this->type;
   return val;
 }
