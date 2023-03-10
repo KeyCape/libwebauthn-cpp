9d5d680e57cc9cf2f4601488c18318d6eb92c812
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:43 +0100

    Add pubKeyCredParams

--- src/PublicKeyCredentialCreationOptions.cpp
+++ src/PublicKeyCredentialCreationOptions.cpp
@@ -8,14 +8,20 @@ PublicKeyCredentialCreationOptions::PublicKeyCredentialCreationOptions(
     std::shared_ptr<std::forward_list<PublicKeyCredentialParameters>>
         &pubKeyCredParams)
 
-    : rp{rp}, user{user}, challenge{challenge} {}
+    : rp{rp}, user{user}, challenge{challenge}, pubKeyCredParams{
+                                                    pubKeyCredParams} {}
 std::unique_ptr<Json::Value> PublicKeyCredentialCreationOptions::getJson() {
   auto ret = std::make_unique<Json::Value>(Json::objectValue);
 
   (*ret)["rp"] = *this->rp->getJson();
   (*ret)["user"] = *this->user->getJson();
   (*ret)["challenge"] = *this->challenge->getJson();
-  // TODO Add pubKeyCredParams
+
+  auto jsonPubKeyCredParams = Json::Value{Json::arrayValue};
+  for (auto param : *this->pubKeyCredParams) {
+    jsonPubKeyCredParams.append(*param.getJson());
+  }
+  (*ret)["pubKeyCredParams"] = jsonPubKeyCredParams;
   return ret;
 }
 PublicKeyCredentialCreationOptions::~PublicKeyCredentialCreationOptions() {}
