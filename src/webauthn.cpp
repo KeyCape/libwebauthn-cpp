d63a2935c5b9635bc1e7a721d1e08fbfdcfeeb10
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:42 +0100

    Add parametrized constructor

--- src/webauthn.cpp
+++ src/webauthn.cpp
@@ -1,5 +1,8 @@
-#include "../include/webauthn.h"
+#include "webauthn.h"
 
 Webauthn::Webauthn() {}
+
+Webauthn::Webauthn(std::string &&name, std::string &&id)
+    : rp_name{name}, rp_id{id} {}
 
 Webauthn::~Webauthn() {}
